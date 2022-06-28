03b00646e183f4e35383174615d0000fdbc89806
Daniel <ofodiledaniel20@gmail.com>
2022-06-28 14:20:02 -0700

    Creates solution files to python programming ALX task

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'#program that prints number from 0-99 '
+
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
