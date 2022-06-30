6762870a5910e2120d7c0ad9f8223c9eca68904d
Daniel <ofodiledaniel20@gmail.com>
2022-06-30 10:16:50 -0700

    Fixed error on 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 i#!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """
 
     Prints the result of some math operations on two integers.
@@ -9,7 +9,7 @@ if __name__ = "__main__":
     """
     a = 10
     b = 5
-    print({:d} + {:d} = {:d}.format(a, b, add(a, b)))
-    print({:d} - {:d} = {:d}.format(a, b, sub(a, b)))
-    print({:d} × {:d} = {:d}.format(a, b, mul(a, b)))
-    print({:d} / {:d} = {:d}.format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} × {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
