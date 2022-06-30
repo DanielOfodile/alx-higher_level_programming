1a63c2d12ce23efba11f9345051dae8efcc1228e
Daniel <ofodiledaniel20@gmail.com>
2022-06-30 10:21:39 -0700

    fixed errors

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
     print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} × {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
     print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
