764ccced603261ae31f4b9385e6d47d34634d695
Daniel <ofodiledaniel20@gmail.com>
2022-06-30 09:42:03 -0700

    creates 0-add.py

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import add_0.add
+
+if __name__ == "__main__":
+    """
+
+    Args:
+    a = first integer
+    b = second integer
+    add(a, b) = addition function
+
+    Returns:
+    The result of the addition between two numbers
+
+    """
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
