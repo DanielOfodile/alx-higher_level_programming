0eae36ef106bac48ce14ba8ce24ddab184a98ca9
Daniel <ofodiledaniel20@gmail.com>
2022-07-06 15:24:55 -0700

    rewrites 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            i = replace
-    return my_list
+    return [replace if i == search else i for i in my_list]
