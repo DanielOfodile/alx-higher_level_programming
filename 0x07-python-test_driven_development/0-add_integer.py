22ff6b7cba5e77efc9942a4b244eb41c1fe094e9
Daniel <ofodiledaniel20@gmail.com>
2022-07-22 09:24:47 -0700

    Module to find the max integer in a list

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,35 +1,50 @@
 #!/usr/bin/python3
-"A module to add two numbers
+"""A module to add two numbers
 
 This module performs the addition operation between two numbers,
 these numbers can be integers or floats.
 
 """
 
+
 def add_integer(a, b=98):
     """Adds two numbers
 
     Performs the addition between two numbers.
-    
+
     Args:
         a (:obj:`int, float`): The first number.
         b (:obj:`int, float`, optional): The second number.
-    
+
     Returns:
         int: The result of the addition.
 
     """
-
-    if a is not type(int or float):
+    if type(a) not in (int, float):
         raise TypeError('a must be an integer')
-    
-    if b is not type(int or float):
-        raise TypeError('b must be an integer')
-    
-    if a is type(float):
-        a = int(a)
-    
-    if b is type(float):
-        b = int(b)
 
-return  a + b
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+
+    a = convert_to_int(a)
+    b = convert_to_int(b)
+    return a + b
+
+
+def convert_to_int(num):
+    """Cast the data type of num parameter
+
+    Convert a float number to a integer number
+
+    Args:
+        num (:obj:`int, float`): The number to cast.
+
+    Returns:
+        int: The number casted to integer.
+
+    """
+    if type(num) is float:
+        num = int(num)
+        return num
+
+    return num    
