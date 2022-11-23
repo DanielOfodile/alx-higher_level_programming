f71bee2dbaece598e83f43259f91e5195b9568e7
Daniel Ofodile <ofodiledaniel20@gmail.com>
2022-11-23 18:25:58 +0000

    Upload all jQuery initial tasks

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,15 @@
-$('document').ready(function () {
-  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-    $('DIV#hello').text(data.hello);
+$(document).ready(function () {
+  const salutUri = 'https://fourtonfish.com/hellosalut/?lang=fr';
+  const $helloElement = $('div#hello');
+
+  function getSalut () {
+    return $.get({
+      url: salutUri,
+      dataType: 'json'
+    });
+  }
+
+  getSalut().then((res) => {
+    $helloElement.text(res.hello);
   });
 });
